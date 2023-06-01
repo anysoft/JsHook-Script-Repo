0e332728c2775ed81f113146d979f9e980c2c716
bcmdy <48650902+bcmdy@users.noreply.github.com>
2023-06-02 00:18:44 +0800

    更新商店

--- buildstore.js
+++ buildstore.js
@@ -41,7 +41,7 @@ function main() {
             // console.log(filepatn);
             let mtime = fs.statSync(filepatn + scriptType + ".js").mtime.toLocaleString('zh-CN', { timeZone: 'Asia/Shanghai' })
             // console.log(mtime);
-            let branch = "main"; // master
+            let branch = "master"; // master
             let store = {
                 "author": author,
                 "markdown": "https://raw.githubusercontent.com/bcmdy/JsHook-Script-Repo/" + branch + "/Scripts/" + name + "/README.md",
